c7b4610178ef6d84379734021f9f59fa80a03530
hyunwoome <hyunwoome@icloud.com>
2021-01-23 20:47:10 +0900

    Connect Mongodb Atlas

--- server/app.js
+++ server/app.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const app = express();
+const connectDB = require('./db');
+require('dotenv').config();
+
+const port = process.env.PORT || 8082;
+
+connectDB();
+
+app.use('/', (req, res) => {
+	res.send('Hello World');
+});
+
+app.listen(port, () => {
+	console.log(`Server Running: ${port}`);
+});
